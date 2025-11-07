ac34cb6081b5f06fb676ac33b3abc9bb0c76f626
Serj Elokhin <s.elokhin@rightbrain.pro>
2025-11-07 18:47:12 +0300

    Support form field.

---
+++ lib/src/types.dart
@@ -0,0 +1,11 @@
+enum UnfocusBehavior
+{
+  /// Leave the unsubmitted text out of sync with the value.
+  nothing,
+
+  /// Restore the unsubmitted text from the current value.
+  resetValue,
+
+  /// Save the unsubmitted text to the value.
+  saveValue,
+}
